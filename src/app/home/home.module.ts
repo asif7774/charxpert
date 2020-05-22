493938d36733271c912d88a1ff938d3f233d074d
rahulbarfa11 <rahulbarfa11@gmail.com>
2020-05-22 22:57:58 +0530

    login ui

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, LoginComponent],
+  declarations: [HomeComponent, LoginComponent, SignupComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
