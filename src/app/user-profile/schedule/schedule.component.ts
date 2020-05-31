5558b4ca3ebb70dc2a2e49797894ea9dc1583344
rahulbarfa11 <rahulbarfa11@gmail.com>
2020-05-31 15:28:30 +0530

    ui update

--- src/app/user-profile/schedule/schedule.component.ts
+++ src/app/user-profile/schedule/schedule.component.ts
@@ -13,6 +13,13 @@ export class ScheduleComponent implements OnInit {
 
   viewDate: Date = new Date();
 
+  ProScheduleShow:boolean = true;
+  ProMyprofileShow:boolean = false;
+  ProNotificationShow:boolean = false;
+  ProPaymentShow:boolean = false;
+  ProTransactionShow:boolean = false;
+  ProPreferencesShow:boolean = false;
+
   events: CalendarEvent[] = [
     {
       title: 'No event end date',
@@ -27,6 +34,55 @@ export class ScheduleComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ProSchedule(){
+    this.ProScheduleShow = true;
+    this.ProMyprofileShow = false;
+    this.ProNotificationShow = false;
+    this.ProPaymentShow = false;
+    this.ProTransactionShow = false;
+    this.ProPreferencesShow = false;
+  }
+  ProMyprofile(){
+    this.ProScheduleShow = false;
+    this.ProMyprofileShow = true;
+    this.ProNotificationShow = false;
+    this.ProPaymentShow = false;
+    this.ProTransactionShow = false;
+    this.ProPreferencesShow = false;
+  }
+  ProNotification(){
+    this.ProScheduleShow = false;
+    this.ProMyprofileShow = false;
+    this.ProNotificationShow = true;
+    this.ProPaymentShow = false;
+    this.ProTransactionShow = false;
+    this.ProPreferencesShow = false;
+  }
+  ProPayment(){
+    this.ProScheduleShow = false;
+    this.ProMyprofileShow = false;
+    this.ProNotificationShow = false;
+    this.ProPaymentShow = true;
+    this.ProTransactionShow = false;
+    this.ProPreferencesShow = false;
+  }
+  ProTransaction(){
+    this.ProScheduleShow = false;
+    this.ProMyprofileShow = false;
+    this.ProNotificationShow = false;
+    this.ProPaymentShow = false;
+    this.ProTransactionShow = true;
+    this.ProPreferencesShow = false;
+  }
+  ProPreferences(){
+    this.ProScheduleShow = false;
+    this.ProMyprofileShow = false;
+    this.ProNotificationShow = false;
+    this.ProPaymentShow = false;
+    this.ProTransactionShow = false;
+    this.ProPreferencesShow = true;
   }
 
 }
