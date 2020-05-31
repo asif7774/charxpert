f4ba32a494e6f67152025c8e315398f6701ba7e0
rahulbarfa11 <rahulbarfa11@gmail.com>
2020-05-31 20:51:09 +0530

    ui update

--- src/app/user-profile/user-profile.module.ts
+++ src/app/user-profile/user-profile.module.ts
@@ -8,12 +8,18 @@ import { ScheduleComponent } from './schedule/schedule.component';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
+import { TagInputModule } from 'ngx-chips';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [ReloginComponent, ScheduleComponent],
   imports: [
     CommonModule,
     UserProfileRoutingModule,
-    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
+    TagInputModule, 
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class UserProfileModule { }
